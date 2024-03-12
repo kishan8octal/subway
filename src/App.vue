0c6ef9863671568f18380be6f38f2769782a2dfc
Ajay Makwana <ajay.makwana@eligocs.com>
2024-03-12 14:42:38 +0530

    fix: setup project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeroSection from './pages/HeroSection.vue';
+</script>
+
+<template>
+  <div class="min-h-100vh">
+    <HeroSection />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/pages/HeroSection.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import RadioGroup from '../components/RadioGroup.vue';
+</script>
+<template>
+    <section class="flex items-center h-screen p-5">
+        <div class="container mx-auto py-10 bg-white p-5 shadow-lg rounded-lg">
+            <div class="flex flex-col gap-5">
+                <div>
+                    <h3 class="text-2xl text-lime-300 font-bold text-gray-900">Place Order To Get Deliver From:</h3>
+                </div>
+                <div>
+                    <h1 class="text-4xl font-medium  text-red-500">Subway</h1>
+                </div>
+                <div>
+                    <p class="text-lg text-emerald-700"> 9979 Bustleton Avenue</p>
+                    <p class="text-lg text-emerald-700">Philadelphia PA 19115</p>
+                </div>
+                <div>
+                    <RadioGroup />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style></style>
