9ee54238ccd88d83faa7d2aa172292592e36c823
Ajay Makwana <ajay.makwana@eligocs.com>
2024-04-03 10:21:15 +0530

    fix: add vue x

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { MotionPlugin } from '@vueuse/motion';
 import App from './App.vue';
 import './index.css';
 import router from './router';
+import Vuex from 'vuex';
 
 const app = createApp(App);
 app.use(router);
 app.use(MotionPlugin);
+app.use(Vuex)
 app.mount('#app');
